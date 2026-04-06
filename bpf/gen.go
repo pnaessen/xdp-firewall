0aca6f21064f403cdb2e2c2670a157b9dce2fcf3
pnaessen <pnaessen@student.42lyon.fr>
2026-04-06 11:41:39 -0400

    Update go:generate command in gen.go to include additional include path for BPF compilation

--- bpf/gen.go
+++ bpf/gen.go
@@ -1,3 +1,3 @@
 package bpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go bpf xdp_filter.c -- -I./headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go Bpf xdp_filter.c -- -I./headers -I/usr/include/x86_64-linux-gnu
