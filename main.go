5582c3de3e1ed2356744f083584d3a679f8c127a
pnaessen <pnaessen@student.42lyon.fr>
2026-05-14 10:20:16 -0400

    Refactor main function to use flag for interface name and improve logging messages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -8,13 +9,16 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
 	"github.com/cilium/ebpf/link"
+
 	"github.com/pnaessen/xdp-firewall/bpf"
 )
 
 // XDP firewall entry point: orchestrates eBPF program lifecycle (load, attach, monitor, cleanup).
 func main() {
-	ifaceName := "eth0"
+	ifaceName := flag.String("iface", "eth0", "Network interface to attach XDP program")
+	flag.Parse()
 
 	// Load compiled eBPF bytecode into kernel memory
 	var objs bpf.BpfObjects
@@ -23,9 +27,9 @@ func main() {
 	}
 	defer objs.Close()
 
-	iface, err := net.InterfaceByName(ifaceName)
+	iface, err := net.InterfaceByName(*ifaceName)
 	if err != nil {
-		log.Fatalf("Interface %s not found: %v", ifaceName, err)
+		log.Fatalf("Interface %s not found: %v", *ifaceName, err)
 	}
 
 	// Attach eBPF program to XDP hook
@@ -38,17 +42,26 @@ func main() {
 	}
 	defer l.Close()
 
-	fmt.Printf(" XDP firewall active on %s. Aggregation mode.\n", ifaceName)
+	fmt.Printf("XDP firewall active on %s. Aggregation mode.\n", *ifaceName)
 
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
-	go func() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
-		previousStats := make(map[string]uint64)
+	go monitorStats(quit, ticker, &objs)
 
-		for range ticker.C {
+	<-quit
+	fmt.Println("\nSignal received. Stopping firewall and cleaning up kernel...")
+}
 
+func monitorStats(quit <-chan os.Signal, ticker *time.Ticker, objs *bpf.BpfObjects) {
+	previousStats := make(map[string]uint64)
+
+	for {
+		select {
+		case <-ticker.C:
 			seenIPs := make(map[string]bool)
 			var key [4]byte
 			var cpuValues []uint64
@@ -67,13 +80,13 @@ func main() {
 				delta := currentTotal - prevTotal
 
 				if delta > 0 {
-					fmt.Printf("XDP drop: %d ping block from ip %s (Total ping block: %d)\n", delta, ip, currentTotal)
+					fmt.Printf("XDP drop: %d ping block from ip %s (Total: %d)\n", delta, ip, currentTotal)
 					previousStats[ip] = currentTotal
 				}
 				seenIPs[ip] = true
 			}
 			if err := iterator.Err(); err != nil {
-				log.Printf("Erreur lors de la lecture de la map: %v\n", err)
+				log.Printf("Error reading eBPF map: %v\n", err)
 			}
 
 			for ip := range previousStats {
@@ -81,13 +94,9 @@ func main() {
 					delete(previousStats, ip)
 				}
 			}
+
+		case <-quit:
+			return
 		}
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
-	<-quit
-
-	fmt.Println("\n Signal received. Stopping firewall and cleaning up kernel...")
+	}
 }
